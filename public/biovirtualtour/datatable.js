ea498609a73ff756de6ef3fc3e25b19588491d8b
sigit <sigit@student.untan.ac.ad>
2023-05-09 17:37:05 +0700

    v2

---
+++ public/biovirtualtour/datatable.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#dataTable').DataTable({
+        paging: false,
+        ordering: false,
+        info: false,
+        language: {
+            searchPlaceholder: "Search name room",
+            search: ""}
+            
+    });
+});
